8d40968eef6fddc0c26c9830b898759946a6bf4b
George Owusu <georgeowusu@Georges-MacBook-Pro.local>
2022-06-12 10:56:32 -0400

    user will be able to add an input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import AddTodoForm from './AddTodoForm';
-import TodoList from './TodoList';
+import React from "react";
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./TodoList";
 
 function App() {
-  const [newTodo, setNewTodo] = React.useState('')
+  const [newTodo, setNewTodo] = React.useState("");
   return (
     <div>
       <h1>ToDo List</h1>
