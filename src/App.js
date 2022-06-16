52ea3fa6fab0ad9ee609b0126540a60ba4e85f3e
George Owusu <georgeowusu@georges-mbp.myfiosgateway.com>
2022-06-15 23:25:46 -0400

    created a hook, created side effect

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React from 'react'
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-function App() {
-  const [todoList, setTodoList] = React.useState([]);
-  
-  //Add New Todo to List
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo])
-  }
-  
-  React.useState(localStorage.getItem)
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')));
 
   // Save Todo List in Storage
   React.useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList))
-  }, [todoList])
+  },[todoList]);
+
+  return [todoList, setTodoList];
+}
+
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
+  //Add New Todo to List
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo])
+  }
 
   return (
     <div>
