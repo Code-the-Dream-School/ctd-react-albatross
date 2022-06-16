2a864daaedb2f02318ea1c8bd1348479052f1e6b
dow-mp <derin.mitchell@gmail.com>
2022-06-16 14:03:07 -0400

    pass callback handler from App to AddToDoForm to useState to update component when input submitted

--- src/AddToDoForm.js
+++ src/AddToDoForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-export const AddToDoForm = () => {
+export const AddToDoForm = (props) => {
+    let [newToDo, setNewToDo] = React.useState('');
     const handleAddToDo = (e) => {
         // console.log(e);
         e.preventDefault();
@@ -10,9 +11,12 @@ export const AddToDoForm = () => {
         // can instead use this query Selector method to access the value of the input element directly and store it as toDoTitle - on quick read through, this may be clearer and easier to understand
         let toDoTitle = document.querySelector("input[name='title']").value;
         console.log(toDoTitle);
+        // instructions indicate that onAddToDo should be passed the variable newToDo, however, after trying that it didn't result in anything happening but when passing in the variable within which the input field value is stored - the page updates accordingly
+        // props.onAddToDo(newToDo); --- doesn't work
+        props.onAddToDo(toDoTitle); // prints the value from the input field as expected
         // assigning toDoTitle an empty string does not clear the input field; but assigning the variable stored in toDoTitle as an empty string DOES clear the input field - not sure why as toDoTitle and document.querySelector("input[name='title']").value are the same thing.... 
         // toDoTitle = '';
-        document.querySelector("input[name='title']").value = '';
+        // document.querySelector("input[name='title']").value = '';
     }
     return (
         <form onSubmit={handleAddToDo}>
--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import {AddToDoForm} from './AddToDoForm';
 import {toDoList} from './ToDoList';
 
 function App() {
+  let [newToDo, setNewToDo] = React.useState('');
   return (
     <div>
       <h1>To Do List</h1>
-      <AddToDoForm />
-      
+      <AddToDoForm onAddToDo={setNewToDo}/>
+      <p>{newToDo}</p>
       {/* pass in toDoList to the ToDoList component as props to access the data stored in the toDoList object (ids and titles) */}
       <ToDoList list={toDoList}/>
     </div>
