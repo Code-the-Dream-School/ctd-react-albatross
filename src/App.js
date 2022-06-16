7808660ad897e2fb81c2ede5f653446d4fc813b0
Anna Liskovets <anna.liskovets@Annas-MacBook-Pro.local>
2022-06-15 21:31:15 -0400

    Lesson 1-2. Added ToDo form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from "react";
-
-let todoList = [
-  {
-    id: 1,
-    title: "Read the book",
-  },
-  {
-    id: 2,
-    title: "Go for a walk with the baby",
-  },
-  {
-    id: 3,
-    title: "Send B & R order back",
-  },
-];
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function (item) {
-          return (
-            <li key={item.id}>
-              <span>{item.title}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </div>
   );
 }
