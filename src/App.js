9f11737d6138d7fbb9ed343f38ddb25accac6673
eodero <erickarthurs13@gmail.com>
2022-06-17 13:17:15 -0500

    moved todoList array from App.js to TodoList component.  Refactored App.js to include TodoList component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-
 import React from 'react';
-
-const todoList = [
-  {
-    id: 0,
-    title: 'Read The-Road-to-React'
-  },
-
-  {
-    id: 1,
-    title: 'Complete current assignment'
-  },
-
-  {
-    id: 2,
-    title: 'Submit assignment'
-  }
-];
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
     <div>
       {/* create a level 1 heading Todo List, unordered list and return render Todo List items */}
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function (item) {
-          return (
-            <li key={item.id}>
-              <span>
-                {item.title}
-              </span>
-            </li>
-          )
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
+
     </div>
   );
 }
