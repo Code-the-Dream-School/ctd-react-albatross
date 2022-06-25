78cbbcda6464330ec3dafe3f2d4f24a7290130e5
George Owusu <georgeowusu@georges-mbp.myfiosgateway.com>
2022-06-25 18:53:13 -0400

    created a new state,and crontrolled input which allows user to add new Todo to the list

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,31 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-const AddTodoForm = ({onAddTodo}) => {
-  const [todoTitle, setTodoTitle] = React.useState('');
+const AddTodoForm = ({ onAddTodo }) => {
+	const [todoTitle, setTodoTitle] = React.useState('');
 
-  const handleTitleChange = (event) => {
-    const newTodoTitle = event.target.value
-    setTodoTitle(newTodoTitle)
-  }
+	const handleTitleChange = (event) => {
+		const newTodoTitle = event.target.value;
+		setTodoTitle(newTodoTitle);
+	};
 
-  // Handle "Add Todo Form" Submit
-  const handleAddTodo = (event) => {
-    event.preventDefault();
-    onAddTodo({title: todoTitle, id:Date.now()})
-    setTodoTitle('')
-  }
-  
-  return (
-    <div>
-      <form onSubmit={handleAddTodo}>
-        <label htmlFor='todoTitle'>Title</label>
-        <input id='todoTitle' placeholder='Add Title' name='title' value={todoTitle} onChange={handleTitleChange} > 
-        </input>
-        <button>Add</button>
-      </form>
-      
-    </div>
-  )
-}
+	// Handle "Add Todo Form" Submit
+	const handleAddTodo = (event) => {
+		event.preventDefault();
+		onAddTodo({ title: todoTitle, id: Date.now() });
+		setTodoTitle('');
+	};
 
-export default AddTodoForm
+	return (
+		<div>
+			<form onSubmit={handleAddTodo}>
+				<label htmlFor='todoTitle'>Title</label>
+				<input
+					id='todoTitle'
+					type='text'
+					name='title'
+					value={todoTitle}
+					onChange={handleTitleChange}></input>
+				<button>Add</button>
+			</form>
+		</div>
+	);
+};
+
+export default AddTodoForm;
--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-function App() {
-  const [todoList, setTodoList] = React.useState([]);
-  
-  //Add New Todo to List
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo])
-  }
-  return (
-    <div>
-      <h1>ToDo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList}/>
-    </div>
-  );
-}
+const App = () => {
+	const [todoList, setTodoList] = React.useState([]);
+
+	//Add New Todo to List
+	const addTodo = (newTodo) => {
+		setTodoList([...todoList, newTodo]);
+	};
+	return (
+		<div>
+			<h1>ToDo List</h1>
+			<AddTodoForm onAddTodo={addTodo} />
+			<TodoList todoList={todoList} />
+		</div>
+	);
+};
 
 export default App;
