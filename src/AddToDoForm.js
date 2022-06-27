c26dd7a6e7ea004933f3d100f1728c725d41f301
dow-mp <derin.mitchell@gmail.com>
2022-06-26 22:10:10 -0400

    change form submission callback handler name to match attribute name per best practices/convention

--- src/AddToDoForm.js
+++ src/AddToDoForm.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 export const AddToDoForm = (props) => {
+    // this is considered local state - state can only be passed down (because AddToDoForm has no children this state is not passed anywhere and is therefore local state)
     let [newToDo, setNewToDo] = React.useState('');
-    const handleAddToDo = (e) => {
+    const handleSubmit = (e) => {
         // console.log(e);
         e.preventDefault();
         // used console.log(e) to find that the input is at 0 index of the array of the target elements and can access the value using the following combination of dot and bracket notation - however, this isn't super clear to me just from reading it
@@ -19,8 +20,9 @@ export const AddToDoForm = (props) => {
         // document.querySelector("input[name='title']").value = '';
     }
     return (
-        <form onSubmit={handleAddToDo}>
-            <label htmlFor="toDoTitle">Title</label>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="toDoTitle">Title:</label>
+            {/* per mentor session with FS, he had value attr set to the state variable and onChange attr set to another function handleChange which called on the setState function to set the state variable to the value of the event.target; not sure if this is part of 1-3 or 1-4 assignment */}
             <input id="toDoTitle" name="title" />
             <button type="submit">Add</button>
         </form>
