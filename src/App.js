7d66c6a732063b8c4b671ee96a29a5e12df2de16
Onella <nellywebdev@gmail.com>
2022-06-29 17:07:01 -0400

    Add header, Add an array and Add an unordered list with 3 items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
+const todoList = [
+  { id: 1, 
+    title: "Add To List"
+  },
+  { id: 2, 
+    title: "remove From List"
+  },
+  { id: 3, 
+    title: "completed Assignment"
+  },
+];
+
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div className="App">
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+        <h1>Todo List</h1>
+        <ul>
+            {todoList.map((todoList)=> {
+              return (
+                <li> 
+                  {todoList.key}
+                  {todoList.title}
+              </li>
+              )
+            })}
+        </ul>
+        </header>
     </div>
   );
 }
