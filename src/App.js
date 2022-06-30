866487472077f06e50c347334f7da578d23d898e
eodero <erickarthurs13@gmail.com>
2022-06-29 23:06:37 -0500

    add react state to the parent component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
+
+  // new state variable newTodo(gets the state) with update function setNewTodo
+  const [newTodo, setNewTodo] = React.useState('');
+
   return (
     <div>
       {/* create a level 1 heading Todo List, unordered list and return render Todo List items */}
       <h1>Todo List</h1>
-      <AddTodoForm />
+
+      {/* pass setNewTodo as a call back handler prop */}
+      <AddTodoForm onAddTodo={setNewTodo} />
+
+      {/* paragraph displays the value of newTodo variable */}
+      <p>{newTodo}</p>
       <TodoList />
 
     </div>
