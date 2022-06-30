6ea82a4b2833b59d34b8cd9cc838b896350fdf2c
eodero <erickarthurs13@gmail.com>
2022-06-29 23:02:55 -0500

    added an event handler and prop to the addTodoForm

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
-function AddTodoForm() {
+// add props as a parameter 
+function AddTodoForm(props) {
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+
+        const todoTitle = event.target.title.value;
+        // invoke the onAddTodo callback prop and pass newTodo-this is the todoTitle as an argument 
+        props.onAddTodo(todoTitle);
+
+        console.log(todoTitle)
+
+        event.target.reset();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor='todoTitle'>Title</label>
-            <input id='todoTitle'></input>
+            <input id='todoTitle' name='title' />
             <button type='submit'>Add</button>
         </form>
     )
