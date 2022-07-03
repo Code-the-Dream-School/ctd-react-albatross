6e8787810d3d592f969ac58b4ccb756e890aba63
Sherose Badruddin <sheroseb@gmail.com>
2022-07-03 17:11:49 -0400

    replacement for lesson-1-2, added AddTodoForm and TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -19,6 +19,6 @@ function TodoList() {
     )
 }
 
-
-
 export default TodoList;
+
+// Submitting as complete and adding a comment here to make lesson-1-2-b different from lesson-1-2. Will remove when I hear back from mentor and can correct the 10k pending changes issue from lesson-1-2. Will proceed to work on lesson-1-3  
