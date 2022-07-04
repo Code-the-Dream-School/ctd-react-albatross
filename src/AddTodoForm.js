1ccc67ed76c1af33afed8a38aebecf17e5317afa
Amy Sandoval <amysandoval92@gmail.com>
2022-07-03 19:32:00 -0700

    Handle 'Add Todo Form' submit

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddTodoForm = () => {
+const AddTodoForm = (props) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = inputValue;
+    console.log(todoTitle);
+    props.onAddTodo(todoTitle);
+
+    event.target.reset();
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Title</label>
-        <input id="todoTitle" type="text"></input>
+        <input
+          id="todoTitle"
+          type="text"
+          name="title"
+          placeholder="Add your todo here"
+          onChange={handleChange}
+        ></input>
         <input type="submit" value="Add"></input>
       </form>
     </>
