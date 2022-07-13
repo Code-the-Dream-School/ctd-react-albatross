fbb879bd99a7c56cfab91928cbdf644d5a1ff9b8
Amy Sandoval <amysandoval92@gmail.com>
2022-07-12 21:40:09 -0700

    Move Todo List into State, add new Todo to list, and Destructure Props.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,17 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = (props) => (
-  <>
-    <ul>
-      {props.list.map((item) => (
-        <TodoListItem key={item.id} item={item} />
-      ))}
-    </ul>
-  </>
-);
+const TodoList = (props) => {
+  const { todoList } = props;
 
+  return (
+    <>
+      <ul>
+        {todoList.map((item) => (
+          <TodoListItem key={item.id} item={item} />
+        ))}
+      </ul>
+    </>
+  );
+};
 export default TodoList;
