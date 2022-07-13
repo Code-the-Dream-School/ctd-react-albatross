fe6ebd7b4b4e121eb966dfe96212c39b1f862729
George Owusu <georgeowusu@georges-mbp.myfiosgateway.com>
2022-07-12 21:08:37 -0400

    The todo list can now be stored in local storage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')));
+	const [todoList, setTodoList] = React.useState(
+		JSON.parse(localStorage.getItem('savedTodoList'))
+	);
 
-  // Save Todo List in Storage
-  React.useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
-  },[todoList]);
+	React.useEffect(() => {
+		localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+	}, [todoList]);
 
-  return [todoList, setTodoList];
-}
+	return [todoList, setTodoList];
+};
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
-  //Add New Todo to List
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo])
-  }
+	const [todoList, setTodoList] = useSemiPersistentState();
 
-  return (
-    <div>
-      <h1>ToDo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList}/>
-    </div>
-  );
+	const addTodo = (newTodo) => {
+		setTodoList([...todoList, newTodo]);
+	};
+
+	return (
+		<div>
+			<h1>ToDo List</h1>
+			<AddTodoForm onAddTodo={addTodo} />
+			<TodoList todoList={todoList} />
+		</div>
+	);
 }
 
 export default App;
