056d8d443276109bae604ecb62ea4748751bd021
Anna Liskovets <anna.liskovets@Annas-MacBook-Pro.local>
2022-07-12 21:41:46 -0400

    Added custom hook

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 import { useState } from "react";
+import { useEffect } from "react";
+
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("savedTodoList"))
+  );
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+}
 
 const App = () => {
-  // const [newTodo, setNewTodo] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  // const savedList = JSON.parse(localStorage.getItem("savedTodoList"));
+  const [todoList, setTodoList] = useSemiPersistentState();
+
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
 
+  //console.log(savedList);
   return (
     <div>
       <h1>Todo List</h1>
