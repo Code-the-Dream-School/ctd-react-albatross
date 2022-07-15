51d9d08533b15a30a57990add7fb78a00de4c380
Eric Arizmendi <ericlz.25412@gmail.com>
2022-07-14 23:55:11 -0700

    Add new task on click

--- src/App.js
+++ src/App.js
@@ -35,30 +35,4 @@ function App() {
 export default App;
 
 
-// import React from 'react';
-// import TodoList from './TodoList';
-// import AddTodoForm from './AddTodoForm';
-
-// function App() {
-//   const [todoList, setTodoList] = React.useState([]);
-
-//   function addTodo(newTodo) {
-
-//     setTodoList([...todoList, newTodo])
-//   }
-
-//   return (
-//     <div>
-//       <h1>Todo List</h1>
-
-//       <AddTodoForm onAddTodo={addTodo} />
-
-
-//       <TodoList todoList={todoList} />
-
-//     </div>
-//   );
-// }
-
-// export default App;
 
