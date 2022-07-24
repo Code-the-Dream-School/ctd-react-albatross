e8504971f5026c031d84be8d9a6d4484008fb90e
eodero <erickarthurs13@gmail.com>
2022-07-24 15:33:13 -0500

    added onClick prop to TodoListItem component

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function TodoListItem({ title }) {
+
+function TodoListItem({ title, onRemoveTodo }) {
+
     return (
         <li>
             <span>
                 {title}
             </span>
+            <button type="button" onClick={() => onRemoveTodo(title)} >
+                Remove
+            </button>
         </li>
     )
 }
