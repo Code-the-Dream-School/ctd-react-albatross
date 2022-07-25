554af7d1495de1978678fe802517bd3764205549
Eric Arizmendi <ericlz.25412@gmail.com>
2022-07-25 14:03:56 -0700

    custom react hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ToDoList from './TodoList';
 import AddTodoFrom from './AddTodoForm';
 import './App.css';
 
 
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem('savedTodoList') || '[]')
+  );
+
+  useEffect(() => {
+    const json = JSON.stringify(todoList);
+    localStorage.setItem('savedTodoList', json);
+  }, [todoList]);
+  return [todoList, setTodoList];
+};
+
 function App() {
 
   // const [newTodo, setNewTodo] = useState("");
 
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState([]);
 
   const addTodo = newTodo => {
-    setTodoList([...todoList ,newTodo]) 
-  } 
+    setTodoList([...todoList, newTodo])
+  }
 
   return (
-    <div className='App'>
-      <ToDoList
-      todoList={todoList}
-      />
-      <AddTodoFrom
-        // onAddTodo={setNewTodo}
-        onAddTodo={addTodo}
-      />
-      <p>
-        {/* {newTodo} */}
-      </p>
-    </div>
-
+    <>
+      <div className='App'>
+        <ToDoList
+          todoList={todoList}
+        />
+        <AddTodoFrom
+          // onAddTodo={setNewTodo}
+          onAddTodo={addTodo}
+        />
+        <p>
+          {/* {newTodo} */}
+        </p>
+      </div>
+    </>
 
   );
 }
