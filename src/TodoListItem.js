aa8fa296e43c8ffd9ac42573440e96e38d9dac6b
Sherose Badruddin <sheroseb@gmail.com>
2022-08-04 14:58:15 -0400

    Lesson 1.7

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const TodoListItem = ({ todo, onRemoveTodo })=>{
     return (
         <>
-            <li key={todo.id}>{todo.title} <button type="button" onClick={() => onRemoveTodo(todo.id)}>Remove</button></li> 
+            <li key={todo.id}><button type="button" onClick={() => onRemoveTodo(todo.id)}>Remove</button>{todo.title}</li> 
         </>
     );
 };
