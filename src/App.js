7f27cad50a8f66b74dbe4225e38f9a8f52ef7bfb
Amy Sandoval <amysandoval92@gmail.com>
2022-08-11 14:32:24 -0700

    Retrieve list data asynchronously

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ const App = () => {
     setTodoList([...todoList, ...newTodo]);
   };
 
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
-  );
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(() =>
+        resolve({
+          data: { todoList: JSON.parse(localStorage.getItem("savedTodoList")) },
+        })
+      );
+    }, 2000).then((result) => {
+      setTodoList(result.data.todoList);
+    });
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
