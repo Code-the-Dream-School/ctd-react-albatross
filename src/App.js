2ab34f2169459b3f57d3e4b8b138a95d6c5fac0a
George Owusu <georgeowusu@georges-mbp.myfiosgateway.com>
2022-08-16 07:05:26 -0400

    Installed React Router, Setup Router, Added New Route

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@ import React, { useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 import './TodoListItem.module.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 	const [todoList, setTodoList] = React.useState([]);
 	const [isLoading, setIsLoading] = React.useState(true);
 
-	const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`
+	const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`;
 
 	const options = {
 		method: 'Get',
 		headers: {
 			authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-		}
-	}
+		},
+	};
 
 	useEffect(() => {
 		fetch(url, options)
 			.then((response) => response.json())
 			.then((data) => {
-					setTodoList(data.records);
-					setIsLoading(false);
-				})
+				setTodoList(data.records);
+				setIsLoading(false);
+			});
 	});
-	
+
 	useEffect(() => {
 		if (!isLoading) {
 			localStorage.setItem('savedTodoList', JSON.stringify(todoList));
@@ -41,16 +42,24 @@ function App() {
 	};
 
 	return (
-		<div>
-			<h1>ToDo List</h1>
-			<AddTodoForm onAddTodo={addTodo} />
-			<hr />
-			{isLoading ? (
-				<p>Loading ...</p>
-			) : (
-				<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-			)}
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<div>
+							<h1>Todo List</h1>
+							<AddTodoForm onAddTodo={addTodo} />
+							{isLoading ? (
+								<p>Loading...</p>
+							) : (
+								<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+							)}
+						</div>
+					}></Route>
+				<Route path='/new' element={<h1>New Todo List</h1>}></Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
