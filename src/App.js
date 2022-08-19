287270d582c8fd056dabbcb4cd2873a57bd7e07b
Eric Arizmendi <ericlz.25412@gmail.com>
2022-08-18 22:10:25 -0700

    lesson1.7

--- src/App.js
+++ src/App.js
@@ -4,23 +4,45 @@ import AddTodoFrom from './AddTodoForm';
 import './App.css';
 
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem('savedTodoList') || '[]')
-  );
+// const useSemiPersistentState = () => {
+//   const [todoList, setTodoList] = useState(
+//     JSON.parse(localStorage.getItem('savedTodoList') || '[]')
+//   );
 
-  useEffect(() => {
-    const json = JSON.stringify(todoList);
-    localStorage.setItem('savedTodoList', json);
-  }, [todoList]);
-  return [todoList, setTodoList];
-};
+//   useEffect(() => {
+//     const json = JSON.stringify(todoList);
+//     localStorage.setItem('savedTodoList', json);
+//   }, [todoList]);
+//   return [todoList, setTodoList];
+// };
 
 function App() {
 
-  // const [newTodo, setNewTodo] = useState("");
+  //  const [todoList, setTodoList] = useState(
+  //   JSON.parse(localStorage.getItem('savedTodoList') || '[]')
+  // );
 
-  const [todoList, setTodoList] = useSemiPersistentState([]);
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    new Promise((resolve, reject) =>
+    setTimeout(
+      () => resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList') || '[]') } }),
+      2000
+    )
+  )
+    .then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    })
+}, []);
+
+useEffect(() => {
+  if (!isLoading) {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }
+}, [todoList])
 
   const addTodo = newTodo => {
     setTodoList([...todoList, newTodo])
@@ -35,18 +57,15 @@ function App() {
 
   return (
     <>
-      <div className='App'>
-        <ToDoList
-          todoList={todoList}
-          onRemoveTodo={removeTodo}
-        />
+         <div className='App'>
+         {isLoading ? (<p>Loading ...</p>) :
+        (<ToDoList todoList={todoList} onRemoveTodo={removeTodo} />)
+        }
         <AddTodoFrom
           // onAddTodo={setNewTodo}
           onAddTodo={addTodo}
         />
-        <p>
-          {/* {newTodo} */}
-        </p>
+       
       </div>
     </>
 
