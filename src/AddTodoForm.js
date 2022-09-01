1037e8ede5eeddf2e80fdb327bfb4e3b6d239c74
Roy Mosby <ego.madking@gmail.com>
2022-09-01 08:41:11 -0400

    add handleTodo & pass value back up to parent

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-export default function AddTodoForm(){
+export default function AddTodoForm(props){
+
+  function handleAddTodo(event){
+    event.preventDefault()
+    const todoTitle = event.target.title.value
+    props.onAddTodo(todoTitle)
+    console.log(todoTitle)
+    event.target.title.value = ""
+  }
   return (
-    <form>
-      <label htmlFor="title">Title</label>
-      <input type="text" id="todoTitle" />
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="title" >Title</label>
+      <input type="text" id="todoTitle" name="title" />
       <button type="submit">Add</button>
     </form>
   )
