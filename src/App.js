ef6031aff42524c376340f4d93ee03ff17ea0efe
Roy Mosby <ego.madking@gmail.com>
2022-09-05 21:07:26 -0400

    add removeTodo feature

--- src/App.js
+++ src/App.js
@@ -20,11 +20,15 @@ function App() {
     setTodoList([...todoList, newTodo]);
   }
 
+  function removeTodo(id) {
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+  }
+
   return (
     <div>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
     </div>
   );
 }
