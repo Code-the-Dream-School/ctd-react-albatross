8ec3a503561a35cb29bf63e48ce8faf706b83c18
Roy Mosby <ego.madking@gmail.com>
2022-09-05 21:08:42 -0400

    add remove button

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-export default function ToDoListItem({ todo }) {
-  const { title } = todo;
-  return <li>{title}</li>;
+export default function ToDoListItem({ todo, onRemoveTodo }) {
+  const { title, id } = todo;
+  return (
+    <li>
+      {title}
+      <button onClick={() => onRemoveTodo(id)}>Remove</button>
+    </li>
+  );
 }
