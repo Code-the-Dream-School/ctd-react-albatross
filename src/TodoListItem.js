31e7bcb4a0795003ca6e5a36cacd38defe39e02c
Onella <nellywebdev@gmail.com>
2022-09-07 18:07:18 -0400

    Add package.json file, Add index.js file, Add side effects, Add custom hooks and Fragments

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -3,10 +3,9 @@ import React from "react";
 
 const TodoListItem = (props) => {
     return (
-        <div> 
-            {props.id}
+        <li> 
             {props.title}
-        </div>
+        </li>
     )
 }
 
