0b5b503fb0bef3deb303c63e9d4fbf997dfaeb22
Amy Sandoval <amysandoval92@gmail.com>
2022-09-12 22:42:59 -0700

    Install react-icons and add trash icon to TodoListItem component

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,12 +1,13 @@
 import React from "react";
 import style from "./TodoListItem.module.css";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const TodoListItem = ({ todo, onRemoveTodo }) => {
   return (
     <li className={style.ListItem}>
       <span className={style.text}>{todo.title}</span>
       <button onClick={() => onRemoveTodo(todo.id)} className={style.btn}>
-        Delete
+        <FaRegTrashAlt className={style.trash} />
       </button>
     </li>
   );
