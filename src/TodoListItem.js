30d8943352815276d6f42a929db86e27308bd6fe
Roy Mosby <ego.madking@gmail.com>
2022-09-18 11:30:47 -0400

    add style from module

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './TodoListItem.module.css';
 
 export default function ToDoListItem({ todo, onRemoveTodo }) {
   const { title, id } = todo;
   return (
-    <li>
+    <li className={style.ListItem}>
       {title}
       <button onClick={() => onRemoveTodo(id)}>Remove</button>
     </li>
