929be0ba34009ea63e571db528bffb9fd22b60f1
Roy Mosby <ego.madking@gmail.com>
2022-09-18 11:30:27 -0400

    add global styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+//applies global styles
+import styles from './index.css';
+
 const root = createRoot(document.getElementById('root'));
 
 root.render(
