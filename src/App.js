12ecbbc9f024bda8429ca144f2edf3bae2b178b8
Onella <nellywebdev@gmail.com>
2022-09-28 12:15:58 -0400

    Add Asynchronous data, Add conditional rendering, Add advanced state and impossible state

--- src/App.js
+++ src/App.js
@@ -3,24 +3,38 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 // import TodoListItem from './TodoListItem';
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem('savedTodoList')) || []
-  );
-  useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-  }, [todoList]);
-
-  return [todoList, setTodoList];
-};
 
 function App() {
 
-const [todoList, setTodoList]= useSemiPersistentState();
+
+const [todoList, setTodoList] = useState([]);
+ 
+const [isLoading, setIsLoading] = useState(true);
+
+useEffect(() => {
+  new Promise((resolve, reject) =>
+  setTimeout(
+    () =>resolve({data: {todoList: JSON.parse(localStorage.getItem('savedTodoList') || '[]'
+  ) } } ), 
+  2000
+    )
+  )
+
+  .then(result => {
+    setTodoList(result.data.todoList);
+    setIsLoading(false);
+  })
+}, []);
+
+useEffect(() => {
+  if (!isLoading) { 
+  localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }
+},[todoList, isLoading])
 
 const addTodo = (newTodo) => {
   setTodoList([...todoList, newTodo])
-};
+}
 
 const removeTodo = (id) => {
   const newTodoList = todoList.filter((todo) => todo.title !== id);
@@ -30,12 +44,13 @@ const removeTodo = (id) => {
 
   return (
     <div className="App">
-      
+
+      {isLoading ? (<p>Loading ...</p>):
+      (<TodoList todoList={todoList}
+        onRemoveTodo={removeTodo}/>)
+      }
         <AddTodoForm
         onAddTodo={addTodo}/>
-
-        <TodoList todoList={todoList}
-        onRemoveTodo={removeTodo}/>
     </div>
   );
 }
