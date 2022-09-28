fc39998eec285b3dab74efe88611c25bf51c926b
Arizmendi <arizmendi@Erics-MacBook-Pro.local>
2022-09-27 18:44:09 -0700

    styles

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import style from './css/TodoListItem.module.css'; // Import css modules stylesheet as styles
 const TodoListItem = ({ todo, onRemoveTodo }) => {
 
 
     return (
-        <li>
+        <li className={style.ListItem}>
             <span>
                 {todo.fields.Title}
             </span>
-            <button type="button" onClick={() => onRemoveTodo(todo.id)} >
+            <button className={style.button} type="button" onClick={() => onRemoveTodo(todo.id)} >
                 Remove
             </button>
         </li>
