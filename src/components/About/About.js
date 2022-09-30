e45d2773a373bd9710000fd2515186c5b06046c2
Amy Sandoval <amysandoval92@gmail.com>
2022-09-30 16:10:17 -0700

    Add profile photo to About section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,11 @@ import style from "./About.module.css";
 const About = () => {
   return (
     <div className={style.background}>
+      <img
+        src="images/profile-photo-pink.jpg"
+        alt="profile"
+        className={style.img}
+      />
       <h1 className={style.header}>About Me</h1>
       <p className={style.p}>
         Hi, I'm Amy! I began learning to code with Code the Dream in January
