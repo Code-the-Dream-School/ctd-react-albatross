46ebb0d2fa08bdef4b4c68c7cd9e6b58fdda7ce3
Amy Sandoval <amysandoval92@gmail.com>
2022-09-30 13:15:07 -0700

    Add media query to background image on Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,16 @@ import style from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div className={style.background}>
-      <div className={style.body}>
-        <h2 className={style.header}>Welcome to my React.js Final Project!</h2>
-        <p className={style.p}>
-          Through Code the Dream School React Front End Class, I learned how to
-          build a Todo app using React. This course utilizes Road to React by
-          Robin Wieruch as its learning material, with supplemental assignments
-          created by curriculum developers. All of my work throughout the course
-          was done in my GitHub repository.
-        </p>
-        {/* <ul className={style.ul}>
+    <div className={style.body}>
+      <h2 className={style.header}>Welcome to my React.js Final Project!</h2>
+      <p className={style.p}>
+        Through Code the Dream School React Front End Class, I learned how to
+        build a Todo app using React. This course utilizes Road to React by
+        Robin Wieruch as its learning material, with supplemental assignments
+        created by curriculum developers. All of my work throughout the course
+        was done in my GitHub repository.
+      </p>
+      {/* <ul className={style.ul}>
           <li>
             <a href="/todolist">Todo List</a>
           </li>
@@ -21,7 +20,6 @@ const Home = () => {
             <a href="/about">About</a>
           </li>
         </ul> */}
-      </div>
     </div>
   );
 };
