d352febfb427a7d1bdc1642a2bab620249a23408
Amy Sandoval <amysandoval92@gmail.com>
2022-10-02 21:22:14 -0700

    Add socials and icons to Home page

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./index.css";
+import { FaHome } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="nav">
       <a href="/" className="site-name">
-        Home
+        <FaHome />
       </a>
       <ul>
         <li>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,64 @@
 import React from "react";
 import style from "./Home.module.css";
+import { FaReact } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className={style.body}>
-      <h2 className={style.header}>Welcome to my React.js Final Project!</h2>
-      <p className={style.p}>
-        Through Code the Dream School React Front End Class, I learned how to
-        build a Todo app using React. This course utilizes Road to React by
-        Robin Wieruch as its learning material, with supplemental assignments
-        created by curriculum developers. All of my work throughout the course
-        was done in my GitHub repository.
-      </p>
-      {/* <ul className={style.ul}>
+    <>
+      <div className={style.welcome}>
+        <h2 className={style.header}>
+          Welcome to my <br />
+          <FaReact /> React.js <br />
+          final project!
+        </h2>
+        <p className={style.p}>
+          Through Code the Dream School React Front End Class, I learned how to
+          build a Todo app using React. This course utilizes Road to React by
+          Robin Wieruch as its learning material, with supplemental assignments
+          created by curriculum developers. All of my work throughout the course
+          was done in my GitHub repository{" "}
+          <a
+            href="https://github.com/SandovalAmy/ctd-react-albatross"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          .
+        </p>
+      </div>
+      <div className={style.social}>
+        Hello World!
+        <img
+          src="images/renato-ramos-puma-S4MjllDvauE-unsplash.jpg"
+          alt=""
+          className={style.img}
+        />
+        <p className={style.connect}>Want to connect?</p>
+        <ul className={style.icons}>
           <li>
-            <a href="/todolist">Todo List</a>
+            <a
+              href="https://github.com/SandovalAmy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a
+              href="https://www.linkedin.com/in/amyjensandoval/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
           </li>
-        </ul> */}
-    </div>
+        </ul>
+      </div>
+    </>
   );
 };
 
