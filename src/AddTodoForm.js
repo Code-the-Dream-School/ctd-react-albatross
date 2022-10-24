33d8ff171296c76308c558d9a8985f42917c205b
Roy Mosby <ego.madking@gmail.com>
2022-10-24 08:34:21 -0400

    correct spelling of htmlFor

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AddTodoForm(){
   return (
     <form>
-      <label htmlFor="title">Title</label>
+      <label htmlFor="todoTitle">Title</label>
       <input type="text" id="todoTitle" />
       <button type="submit">Add</button>
     </form>
